dbe37d77250cab7f49b1ef372dba6a5248428137
stuardo asus-tuf <stuardo.lucho@pucp.edu.pe>
2023-03-31 17:15:25 -0500

    proyecto base spring security

--- src/main/java/com/example/clase9webservice/controller/ProductoController.java
+++ src/main/java/com/example/clase9webservice/controller/ProductoController.java
@@ -1,15 +1,60 @@
 package com.example.clase9webservice.controller;
 
+import com.example.clase9webservice.entity.Product;
 import com.example.clase9webservice.repository.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
 public class ProductoController {
+
+    @GetMapping("/product")
+    public List<Product> listarProductos() {
+        return productRepository.findAll();
+    }
 
     final ProductRepository productRepository;
 
     public ProductoController(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<HashMap<String,Object>> obtenerProductoPorId(
+            @PathVariable("id") String idStr) {
+
+
+        HashMap<String,Object> responseJson = new HashMap<>();
+
+        try {
+            int id = Integer.parseInt(idStr);
+            Optional<Product> optProduct = productRepository.findById(id);
+            if (optProduct.isPresent()) {
+                responseJson.put("result","success");
+                responseJson.put("product",optProduct.get());
+                return ResponseEntity.ok(responseJson);
+            } else {
+                responseJson.put("msg","Producto no encontrado");
+            }
+        } catch (NumberFormatException e) {
+            responseJson.put("msg","el ID debe ser un número entero positivo");
+        }
+        responseJson.put("result","failure");
+        return ResponseEntity.badRequest().body(responseJson);
+    }
+
+
+
+
+
+
 }
+
---
+++ src/main/java/com/example/clase9webservice/security/WebSecurityConfig.java
@@ -0,0 +1,54 @@
+package com.example.clase9webservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class WebSecurityConfig {
+
+    final DataSource dataSource;
+
+    public WebSecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsManager users(DataSource dataSource){
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+        String sql1 = "SELECT email, pwd, activo FROM usuario WHERE email = ?";
+        String sql2 = "SELECT u.email, r.nombre FROM usuario u "
+                + "INNER JOIN rol r ON (u.idrol = r.idrol) "
+                + "WHERE u.email = ? and u.activo = 1";
+
+        users.setUsersByUsernameQuery(sql1);
+        users.setAuthoritiesByUsernameQuery(sql2);
+        return users;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+
+        http.authorizeHttpRequests()
+                .requestMatchers("/api/product").authenticated()
+                .anyRequest().permitAll();
+
+        return http.build();
+    }
+}
+
+
